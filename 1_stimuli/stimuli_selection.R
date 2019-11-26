ff770d8d692b1a79dec87db0dd30ecc89d0e020d
ameliakimball <amelia.kimball@gmail.com>
2019-11-26 15:01:22 +0100

    updates pair selection script and output

--- 1_stimuli/stimuli_selection.R
+++ 1_stimuli/stimuli_selection.R
@@ -1,65 +1,52 @@
-##
+#!/usr/bin/env Rscript
 #stimuli seleciton pipeline
 
 #created by Amelia 
-#last edit 15 Nov by Amelia 
-#
-#takes the initial narrowed data frame of possible stimuli with acoustic 
-#distances and samples a subset  to form a dataframe of length 
-#150 (= desired number of trials). This new data frame is then input into the
-#optimization script a second time, to select the instances to use (which 
-#recording and which speaker) 
-
-#currently filters on the following things: 
-#1)filter out  velar fricatives entirely
-#2)filter out isntances for which the log acoustic distance ration is <-0.4 
-# (based on pilot data suggesting that lower than this participant 
-#accuracy is at ceiling) 
-
-
-#dependencies:
+#last edit 26 Nov by Amelia 
 
 #library(magrittr)
 #library(readr)
 #library(dplyr)
 
+#takes the initial narrowed data frame of possible stimuli with acoustic 
+#distances and samples a subset  to form a dataframe of length 
+#144 (= desired number of trials). This new data frame of pairs is then made 
+#intro triplets, and then input into the optimization script a second time, to 
+#select the instances to use (which recording and which speaker) 
 
 
 `%>%` <- magrittr::`%>%`
 
-
-# ARGS <-ARGS <- commandArgs(TRUE)
+ ARGS <-ARGS <- commandArgs(TRUE)
 
 # Input triplet file
-INPUT <- "preregistration/triplets.csv"# ARGS[1] 
+PAIRS <- ARGS[1] #"1_stimuli/distances_by_pair.csv" 
 
 # Output filtered subset of these 
-OUTPUT <- "preregistration/exp_length_stim_list.csv" #ARGS[2]
+OUTPUT <- ARGS[2]# "1_stimuli/exp_length_pairs.csv" #
 
-  
-hindi_triplets <- readr::read_csv(INPUT)
-
+pairs_w_dist <- readr::read_csv(PAIRS)
 
 
+#English sounds the most variable between instances are b,z,d͡ʒ ɽʱ
+#once these are removed, remove pairs with the largest mean distance 
 
-hindi_150 <- dplyr::filter(hindi_triplets, phone_TGT != "ɣ"&
-                                  phone_OTH != "ɣ" &
-                                  phone_X != "ɣ") %>%
-             dplyr::filter(., phone_TGT != "x"&
-                                  phone_OTH != "x" &
-                                  phone_X != "x") %>%
-  
-             dplyr::mutate(., ratio_distance=distance_TGT/distance_OTH) %>% 
-             dplyr::mutate(., log_ratio_distance= 
-                             log(distance_TGT/distance_OTH))%>%
-             dplyr::filter(., log_ratio_distance > -.4) %>%
-             dplyr::mutate(., phone_pair =paste0(as.character(phone_TGT),
-                                                 "_",
-                                                 as.character(phone_OTH))) %>%
-             dplyr::group_by(., phone_pair)%>%
-             dplyr::sample_n(size = 1) %>%
-             dplyr::ungroup() %>%
-             dplyr::sample_n(size = 150)
+TGT_OTH_stats <-dplyr::filter(pairs_w_dist,target_other=="OTH")%>%
+  dplyr::filter(phone_1!="z"&phone_2!="z")%>%
+  dplyr::filter(!(phone_1=="ɽʱ" & phone_2 == "d͡ʒ"))%>%
+  dplyr::filter(!(phone_1=="d͡ʒ"& phone_2 =="ɽʱ"))%>%
+  dplyr::group_by(.,phone_1,phone_2, language_1)%>%
+  dplyr::summarise(.,mean_dist = mean(distance),sd_dist =sd(distance)) %>%
+  dplyr::filter(., mean_dist<.375) %>%
+  dplyr::filter(., language_1=="HIN")
 
+# this yields 144 hindi pairs. 
+#when we join, that's doubled to 288 because there are two instances of each pair,
+#so we cut in half again
 
-readr::write_csv(hindi_150, OUTPUT)
+filtered_pairs <- dplyr::left_join(TGT_OTH_stats,pairs_w_dist,
+                                   by = c("phone_1","phone_2","language_1")) %>%
+                  dplyr::group_by(., phone_1, phone_2)%>%
+                  dplyr::sample_n( 1)
+
+readr::write_csv(filtered_pairs, OUTPUT)
