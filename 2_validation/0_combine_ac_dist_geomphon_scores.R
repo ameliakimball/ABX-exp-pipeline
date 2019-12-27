378adb3cb3b22f76ad79318642ce5286b1e3239c
ameliakimball <amelia.kimball@gmail.com>
2019-12-27 20:40:14 +0100

    adds documentation and format script

---
+++ 2_validation/0_combine_ac_dist_geomphon_scores.R
@@ -0,0 +1,24 @@
+#0 combine acoustic distances and hindi scores 
+
+`%>%`<-magrittr::`%>%`
+
+DESIGN_4each <- "2_validation/design_4_each.csv"
+DESIGN_3each <- "2_validation/design_3_each.csv"
+HINDI_SCORES <- "2_validation/hindi_scores.csv"
+
+hindi_scores <- readr::read_csv(HINDI_SCORES) 
+hindi_scores <- dplyr::rename(hindi_scores,Phone_NOTENG=phone)
+
+diff_pairs_4 <- readr::read_csv(DESIGN_4each)
+diff_pairs_4 <- dplyr::rename(diff_pairs_4,Phone_NOTENG=phone_HIN, Phone_ENG=phone_ENG)
+
+
+diff_pairs_3 <- readr::read_csv(DESIGN_3each)
+diff_pairs_3 <- dplyr::rename(diff_pairs_3,Phone_NOTENG=phone_HIN, Phone_ENG=phone_ENG)
+
+new_des_4<- dplyr::left_join(diff_pairs_4,hindi_scores,
+                             by="Phone_NOTENG")
+
+new_des_3<- dplyr::left_join(diff_pairs_3,hindi_scores,
+                             by="Phone_NOTENG")
+
